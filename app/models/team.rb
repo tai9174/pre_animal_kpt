f418071106905b4af6f95a221cc9d944afbdfd2d
tai9174 <104898840+tai9174@users.noreply.github.com>
2022-08-22 09:13:36 +0900

    teamとuserのアソシエーションmodelの変更

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,2 +1,6 @@
 class Team < ApplicationRecord
+  validates :team_name, presence: true, uniqueness: true
+  has_many :join_teams, dependent: :destroy
+  has_many :join_team_users, through: :join_teams, source: :user
+  belongs_to :user
 end
