18f1832de0175a3b81edbfe72597b79771e5908c
tai9174 <104898840+tai9174@users.noreply.github.com>
2022-08-23 10:09:27 +0900

    過去のKPTが見れるようにする

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -95,7 +95,82 @@ class TeamsController < ApplicationController
       end
     end
   end
+
+  def kpts_2
+    @team = Team.find(params[:team_id])
+    @join_team_users= @team.join_team_users
+    @day_2_kpts=[]
+    @join_team_users.each do |join_team_user|
+      join_team_user.kpts.each do |kpt_2|
+        if kpt_2.date.strftime('%Y/%m/%d')== (Time.now.-2.day).strftime('%Y/%m/%d')
+          @day_2_kpts<< kpt_2
+        end
+      end
+    end
+  end
   
+  def kpts_3
+    @team = Team.find(params[:team_id])
+    @join_team_users= @team.join_team_users
+    @day_3_kpts=[]
+    @join_team_users.each do |join_team_user|
+      join_team_user.kpts.each do |kpt_3|
+        if kpt_3.date.strftime('%Y/%m/%d')== (Time.now.-3.day).strftime('%Y/%m/%d')
+          @day_3_kpts<< kpt_3
+        end
+      end
+    end
+  end
+  
+  def kpts_4
+    @team = Team.find(params[:team_id])
+    @join_team_users= @team.join_team_users
+    @day_4_kpts=[]
+    @join_team_users.each do |join_team_user|
+      join_team_user.kpts.each do |kpt_4|
+        if kpt_4.date.strftime('%Y/%m/%d')== (Time.now.-4.day).strftime('%Y/%m/%d')
+          @day_4_kpts<< kpt_4
+        end
+      end
+    end
+  end
+  def kpts_5
+    @team = Team.find(params[:team_id])
+    @join_team_users= @team.join_team_users
+    @day_5_kpts=[]
+    @join_team_users.each do |join_team_user|
+      join_team_user.kpts.each do |kpt_5|
+        if kpt_5.date.strftime('%Y/%m/%d')== (Time.now.-5.day).strftime('%Y/%m/%d')
+          @day_5_kpts<< kpt_5
+        end
+      end
+    end
+  end
+
+  def kpts_6
+    @team = Team.find(params[:team_id])
+    @join_team_users= @team.join_team_users
+    @day_6_kpts=[]
+    @join_team_users.each do |join_team_user|
+      join_team_user.kpts.each do |kpt_6|
+        if kpt_6.date.strftime('%Y/%m/%d')== (Time.now.-6.day).strftime('%Y/%m/%d')
+          @day_6_kpts<< kpt_6
+        end
+      end
+    end
+  end
+  def kpts_7
+    @team = Team.find(params[:team_id])
+    @join_team_users= @team.join_team_users
+    @day_7_kpts=[]
+    @join_team_users.each do |join_team_user|
+      join_team_user.kpts.each do |kpt_7|
+        if kpt_7.date.strftime('%Y/%m/%d')== (Time.now.-7.day).strftime('%Y/%m/%d')
+          @day_7_kpts<< kpt_7
+        end
+      end
+    end
+  end
   def calendar
     @team = Team.find(params[:team_id])
     # @team_kpts=@team.join_team_users.kpts
